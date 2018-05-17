7ea8c9e4b99c6f7e16396f683d347191726d59fb
David Eini (Dave) <dave@lig-it.com>
2018-05-17 14:20:25 +0300

    sync

--- src/app/design/drag-n-drop/drag-n-drop.directive.ts
+++ src/app/design/drag-n-drop/drag-n-drop.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDragNDrop]'
 })
 export class DragNDropDirective {
+  constructor() {}
 
-  constructor() { }
+  @HostListener('dragleave', ['$event'])
+  onDragleave(event) {
+    console.log('dragleave', event);
+  }
 
+  @HostListener('dragenter', ['$event'])
+  onDragenter(event) {
+    console.log('dragenter', event);
+  }
+
+  @HostListener('drop', ['$event'])
+  onDrop(event) {
+    console.log('drop', event);
+  }
 }
